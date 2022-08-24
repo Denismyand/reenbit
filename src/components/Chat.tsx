6846afdf550ac489ab456261b03516e8adbcbfb3
NVMate <denys.miand@nure.ua>
2022-08-24 07:24:16 +0300

    Add chat section with a contact and message input.

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { User } from "../utils/types";
+import styles from "../styles/chat.module.css";
+import { MiniProfile } from "./MiniProfile";
 
 export function Chat({ contact }: { contact: User }) {
-  return <></>;
+  const [newMessage, setNewMessage] = useState("");
+
+  return (
+    <div className={styles.chat}>
+      <div className={styles.chatHeader}>
+        <MiniProfile user={contact} />
+        <p className={styles.userName}>{contact.name}</p>
+      </div>
+      <div className={styles.chatWindow}></div>
+      <form
+        className={styles.chatInputSection}
+        onSubmit={(e) => {
+          e.preventDefault();
+          setNewMessage("");
+        }}
+      >
+        <input
+          className={styles.newMessageInput}
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder={"Enter your message"}
+        />
+        <button className={styles.newMessageSendButton} type="submit" />
+      </form>
+    </div>
+  );
 }
